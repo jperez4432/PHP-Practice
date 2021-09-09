e9629bbd40f5596e3432e67c4ed156d2c4af3851
Juan Perez <jperez4432@gmail.com>
2021-09-09 13:11:40 -0500

    php practice

---
+++ site.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+<?php
+ $phrase = "To be or not to be";
+ $age = 25;
+ $gpa = 3.8;
+ $isMale = true;
+
+?>
+</body>
+</html>
