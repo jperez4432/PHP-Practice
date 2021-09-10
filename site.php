5f7db198d1b0c3dcb8d741a1461fb56191c16c53
Juan Perez <jperez4432@gmail.com>
2021-09-10 11:45:08 -0500

    practicing php

--- site.php
+++ site.php
@@ -6,10 +6,27 @@
 </head>
 <body>
 <?php
- $phrase = "To be or not to be";
+ $phrase = "To be or not to be <br>";
  $age = 25;
  $gpa = 3.8;
  $isMale = true;
+
+echo $phrase;
+
+echo strtolower($phrase);
+echo strtoupper($phrase);
+echo strlen($phrase);
+echo $phrase[0];
+echo "Juan"[2];
+
+
+$phrase[0] = "D";
+echo $phrase;
+
+echo str_replace("be", "code", $phrase);
+
+echo substr($phrase, 9, 3);
+
 
 ?>
 </body>
