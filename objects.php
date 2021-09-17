eb28fa73d0be4e8ac50c4c1a799b6354d4bb4bfa
Juan Perez <jperez4432@gmail.com>
2021-09-17 11:05:34 -0500

    completed objects exercise

---
+++ objects.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<body>
+
+
+    <?php
+    class Student {
+        var $name;
+        var $major;
+        var $gpa;
+
+        function __construct($name, $major, $gpa)
+        {
+            $this-> name = $name;
+            $this->major = $major;
+            $this->gpa = $gpa;
+        } 
+        function hasHonors(){
+            if($this->gpa >= 3.5) {
+                return "true";
+            } else 
+            return "false";
+        }
+
+    }    
+        $student1 = new Student("Juan", "Business", 3.9);
+        $student2 = new Student("Miranda", "Sociology", 4.0);
+
+          echo $student1->hasHonors();
+       // echo $student1->name;
+       // echo $student1->gpa;
+       // echo $student2->major;
+        
+    
+    ?>
+
+</body>
+
+</html>
