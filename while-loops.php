108f45ee1fea369c32166bb2aaab95b7280d848c
Juan Perez <jperez4432@gmail.com>
2021-09-16 22:55:12 -0500

    while loops completed

---
+++ while-loops.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+
+<?php
+$index = 1;
+while ($index <= 5) {
+    echo "$index <br>";
+    $index++;
+}
+?>
+
+</body>
+</html>
