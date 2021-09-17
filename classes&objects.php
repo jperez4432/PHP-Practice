bdf4bcdd0f44b43f7517acd3ef5132464dd96aeb
Juan Perez <jperez4432@gmail.com>
2021-09-17 10:52:30 -0500

    classes and objects exercise with constructors

--- classes&objects.php
+++ classes&objects.php
@@ -13,21 +13,23 @@
         var $title;
         var $author;
         var $pages;
+        
+        function __construct($aTitle, $aAuthor, $aPages)
+        {
+            $this->title = $aTitle;
+            $this->author = $aAuthor;
+            $this->pages = $aPages;
+
+        }
     } 
 
-    $book1 = new Book;
-    $book1->title = "Harry Potter";
-    $book1->author = "JK Rowling";
-    $book1->pages = 400;
-
+    $book1 = new Book("Harry Potter", "JK Rowling", 400);
+   
     echo $book1->title;
     echo $book1->author;
     echo $book1->pages;
 
-    $book2 = new Book;
-    $book2->title = "Lord of the Rings";
-    $book2->author = "Tolkien";
-    $book2->pages = 700;
+    $book2 = new Book("Lord of the Rings", "Tolkien", 700);
 
     echo $book2->title;
     echo $book2->author;
